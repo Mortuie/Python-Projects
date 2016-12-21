cf36177c7c655b5429d8c26933fc3dd847511ef0
Leon Boehmer <jimmybob-leon@hotmail.co.uk>
2016-12-21 13:28:48 +0000

    Added some more stuff to the PurpleRain.py file

--- PurpleRain/PurpleRain.py
+++ PurpleRain/PurpleRain.py
@@ -3,31 +3,36 @@ import random
 
 """
 @author lb809 on 20/12/16
+@inspiration https://www.youtube.com/watch?v=KkyIDI6rQJI
 """
 
 # Colours
 PINK = (189, 144, 212)
-WHITE = (255, 255, 255)
 PURPLE = (34, 21, 73)
 
 # global variables
-FPS = 45
-displayHeight = 500
-displayWidth = 800
+FPS = 60
+displayHeight = 900
+displayWidth = 900
 size = (displayWidth, displayHeight)
 clock = pygame.time.Clock()
 
 class rain:
-    def __init__(self, screen, x, y, speed, width, height):
+    def __init__(self, screen, x, y, speed, height):
         self.x = x
         self.y = y
         self.speed = speed
-        self.width = width
+
         self.height = height
         self.colour = PINK
         self.frame = screen
 
-    def update(self):
+        if self.speed > 11:
+            self.width = random.randint(2, 3)
+        else:
+            self.width = random.randint(1, 2)
+
+    def draw(self):
         pygame.draw.rect(self.frame, self.colour, [self.x, self.y, self.width, self.height], 0)
 
     def move(self):
@@ -40,15 +45,16 @@ class rain:
 def main():
     # running variable
     running = True
+
     # things for pygame to initialise
     pygame.init()
     frame = pygame.display.set_mode(size)
-    pygame.display.set_caption("Rain Test")
+    pygame.display.set_caption("Purple Rain")
 
     # object array of rain
     rainDrops = []
     for i in range(500):
-        rainDrops.append(rain(frame, random.randint(0, displayWidth), random.randint(-50, displayHeight), random.randint(10, 13), random.randint(2, 4), 20))
+        rainDrops.append(rain(frame, random.randint(0, displayWidth), random.randint(-50, displayHeight), random.randint(10, 13), random.randint(7, 13)))
 
     while running:
         for event in pygame.event.get():
@@ -60,11 +66,10 @@ def main():
         for rainDroplets in rainDrops:
             rainDroplets.move()
             rainDroplets.checkOffScreen()
-            rainDroplets.update()
+            rainDroplets.draw()
 
         pygame.display.flip()
         clock.tick(FPS)
-
 
 if __name__ == "__main__":
     main()

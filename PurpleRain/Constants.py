e1946148c26df18fa804e986b4f7fd87f663022e
Leon Boehmer <jimmybob-leon@hotmail.co.uk>
2016-12-28 20:28:48 +0000

    Modified some Purple Rain code, cleaned it up. Added a new sub file, which has all the constants in it.

---
+++ PurpleRain/Constants.py
@@ -0,0 +1,13 @@
+"""
+@author lb809 on 12/28/2016
+"""
+
+# Colours
+PINK = (189, 144, 212)
+PURPLE = (34, 21, 73)
+
+# global variables
+FPS = 60
+DISPLAYHEIGHT = 600
+DISPLAYWIDTH = int((16 * DISPLAYHEIGHT) / 9)
+SIZE = (DISPLAYWIDTH, DISPLAYHEIGHT)
--- PurpleRain/Main.py
+++ PurpleRain/Main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import RainDroplet
+import Constants
 
 """
 @author lb809 on 20/12/16
@@ -11,48 +12,36 @@ import RainDroplet
 pygame.init()
 pygame.mixer.init()
 
-# Colours
-PINK = (189, 144, 212)
-PURPLE = (34, 21, 73)
 
-# global variables
-FPS = 60
-displayHeight = 600
-displayWidth = int((16 * displayHeight) / 9)
-size = (displayWidth, displayHeight)
 clock = pygame.time.Clock()
 droplet = pygame.mixer.music.load("rain.mp3")
 pygame.mixer.music.play()
 
 
-
 def main():
-    # running variable
     running = True
 
-    # things for pygame to initialise
-    frame = pygame.display.set_mode(size)
+    frame = pygame.display.set_mode(Constants.SIZE)
     pygame.display.set_caption("Purple Rain")
 
-    # object array of rain
     rainDrops = []
     for i in range(500):
-        rainDrops.append(RainDroplet.rain(frame, random.randint(0, displayWidth), random.randint(-50, displayHeight), random.randint(10, 13), random.randint(7, 13), PINK))
+        rainDrops.append(RainDroplet.rain(frame, random.randint(0, Constants.DISPLAYWIDTH), random.randint(-50, Constants.DISPLAYHEIGHT), random.randint(10, 13), random.randint(7, 13), Constants.PINK))
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        frame.fill(PURPLE)
+        frame.fill(Constants.PURPLE)
 
         for rainDroplets in rainDrops:
             rainDroplets.move()
-            rainDroplets.checkOffScreen(displayWidth, displayHeight)
+            rainDroplets.checkOffScreen(Constants.DISPLAYWIDTH, Constants.DISPLAYHEIGHT)
             rainDroplets.draw()
 
         pygame.display.flip()
-        clock.tick(FPS)
+        clock.tick(Constants.FPS)
 
 if __name__ == "__main__":
     main()
